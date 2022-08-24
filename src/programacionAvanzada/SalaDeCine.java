2ed89802bfacb0dd4df126a11687e61c08f21808
MarielML <marielmarmol1511@outlook.com>
2022-08-24 16:03:58 -0300

    constructor

--- src/programacionAvanzada/SalaDeCine.java
+++ src/programacionAvanzada/SalaDeCine.java
@@ -1,5 +1,28 @@
 package programacionAvanzada;
 
 public class SalaDeCine {
-
+	private Integer cantidadTotalDeButacas;
+	private Integer cantidadDeFilas;
+	private Integer cantidadDeButacasPorFila;
+	private Boolean[][] butacas;
+	
+	public SalaDeCine(Integer cantidadTotalDeButacas, Integer cantidadDeFilas, Integer cantidadDeButacasPorFila) {
+		this.cantidadTotalDeButacas = cantidadTotalDeButacas;
+		this.cantidadDeFilas = cantidadDeFilas;
+		this.cantidadDeButacasPorFila = cantidadDeButacasPorFila;
+		this.butacas = new Boolean[cantidadDeFilas][cantidadDeButacasPorFila];
+		for(int i = 0; i < cantidadDeFilas; i++) {
+			for(int j = 0; i < cantidadDeButacasPorFila; j++) {
+				this.butacas[i][j] = false;
+			}	
+		}
+	}
+	
+	public Boolean consultarSiEstaOcupada(Integer fila, Integer butaca) {
+		Boolean estaOcupada = false;
+		if(this.butacas[fila][butaca] == true) {
+			estaOcupada = true;
+		}
+		return estaOcupada;
+	}
 }
