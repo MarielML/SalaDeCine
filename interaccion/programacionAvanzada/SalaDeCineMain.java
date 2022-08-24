751922201edf762621aec7cf529f9fc193293412
MarielML <marielmarmol1511@outlook.com>
2022-08-24 15:44:04 -0300

    primer commit

---
+++ interaccion/programacionAvanzada/SalaDeCineMain.java
@@ -0,0 +1,10 @@
+package programacionAvanzada;
+
+public class SalaDeCineMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/programacionAvanzada/SalaDeCine.java
@@ -0,0 +1,5 @@
+package programacionAvanzada;
+
+public class SalaDeCine {
+
+}
